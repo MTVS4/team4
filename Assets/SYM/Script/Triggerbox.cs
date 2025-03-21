e53943b0aa25ef8e8ecfd8460d81a683b447e522
demo <85276514+genmoo@users.noreply.github.com>
2025-03-21 18:26:01 +0900

    데칼 기능 업데이트2

--- Assets/SYM/Script/Triggerbox.cs
+++ Assets/SYM/Script/Triggerbox.cs
@@ -11,20 +11,16 @@ public class Triggerbox : MonoBehaviour
 {
     public GameObject decal1;
     public Transform playerObj;
+    public GameObject cube;
     public float playerSpeed = 1f;
     float x,z;
     public float Alpha;
     public float decalAlpha;
-   
     public Material decalMat1;
-   
     DecalProjector cubeDecal;
     public float fadeSpeed;
-
-    public GameObject cube;
-
-
-    // rotate
+    
+    // camera rotate
     [SerializeField]
     float eulerAngX;
     [SerializeField]
@@ -48,6 +44,7 @@ public class Triggerbox : MonoBehaviour
     void Update()
     {
         //  decalMat1.SetFloat("_power",  Alpha); 
+        
         eulerAngX = playerObj.transform.localEulerAngles.x;
         eulerAngY = playerObj.transform.localEulerAngles.y;
         eulerAngZ = playerObj.transform.localEulerAngles.z;
@@ -60,30 +57,30 @@ public class Triggerbox : MonoBehaviour
         // 트리거 박스 플레이어 위치 보정  
        x = playerObj.transform.position.x;
        z = playerObj.transform.position.z;
-       float truncatedx = Mathf.Floor(x * 10f) / 10f;
-       float truncatedz = Mathf.Floor(z * 10f) / 10f;
-       print(truncatedx);
-       print(truncatedz);
-
-      //  플레이어 회전값
-      // playerObj.transform.rotation
-        
-
+       float truncatedx = Mathf.Floor(x * 100f) / 100f;
+       float truncatedz = Mathf.Floor(z * 100f) / 100f;
+ 
         //  데칼 끄고 큐브 키기
         if(playerObj.gameObject.tag =="Trigger")
         { 
-            Debug.Log("stay");
+          //  히트존으로 보정정
           playerObj.transform.position += (transform.position - playerObj.transform.position) * Time.deltaTime * playerSpeed;
         }
             
-            if (Mathf.Approximately(truncatedx, 0.5f) && Mathf.Approximately(truncatedz, 1.0f))
+            if (Mathf.Approximately(truncatedx, 0.51f) && Mathf.Approximately(truncatedz, 1.04f) && 155 < eulerAngY && eulerAngY < 205)
             {
               print("ture"); 
           
-            cube.SetActive(true);
-            decal1.SetActive(false);
+              cube.SetActive(true);
+              decal1.SetActive(false);
+              Destroy(this.gameObject);
             }
             
     }
 
 }
+
+
+
+
+
